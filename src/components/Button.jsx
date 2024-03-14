8bcd8c89c8f5459d526fe10f53d99cb89a96dd91
James Mills <james.mills@lontra.co.uk>
2024-03-14 15:42:46 +0000

    Customised button variants a little more.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 export default function Button({ children, color, variant }) {
     const variantClasses = {
-        filled: "px-4 py-2 rounded bg-slate-600 hover:bg-slate-700 text-xs md:text-base font-semibold transition-all duration-200 ease-in-out",
-        outline: "px-4 py-2 rounded border border-slate-600 hover:bg-slate-600 hover:text-white text-xs md:text-base font-semibold transition-all duration-200 ease-in-out",
-        text: "px-4 py-2 text-xs md:text-base font-semibold transition-all duration-200 ease-in-out",
+        filled: "px-4 py-2 rounded bg-slate-600 hover:bg-slate-700 text-xs md:text-base font-semibold translate-x-0 hover:-translate-y-0.5 transition-all duration-200 ease-in-out",
+        outline: "px-4 py-2 rounded border border-slate-600 hover:bg-slate-600 hover:text-white text-xs md:text-base font-semibold translate-x-0 hover:-translate-y-0.5 transition-all duration-200 ease-in-out",
+        text: "px-4 py-2 rounded text-xs md:text-base font-semibold translate-x-0 hover:-translate-y-0.5 transition-all duration-200 ease-in-out",
     };
 
     const colorClasses = {
-        default: "text-stone-200 hover:text-stone-50",
-        danger: "text-red-500 bg-red-50 hover:bg-red-500 hover:text-white",
-        success: "text-green-500 bg-green-50 hover:bg-green-500 hover:text-white",
+        default: "text-stone-200 hover:text-stone-50 hover:shadow-lg hover:shadow-stone-500/20",
+        danger: "text-red-500 bg-red-50 hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/20",
+        success: "text-green-500 bg-green-50 hover:bg-green-500 hover:text-white hover:shadow-lg hover:shadow-green-500/20",
     };
 
     const variantClassName = variantClasses[variant] || "";
--- src/partials/Project/NewProject.jsx
+++ src/partials/Project/NewProject.jsx
@@ -1,18 +1,19 @@
 import Input from '../../components/Input.jsx'
+import Button from '../../components/Button.jsx'
 
 export default function NewProject() {
-  return (
-    <div className="w-[35rem] mt-16 px-8">
-        <menu className='flex items-center justify-end gap-4 my-4'>
-              <li><button className="text-gray-500 hover:text-red-500 hover:bg-red-50 px-4 py-2 rounded transition-all duration-200 ease-in-out">Cancel</button></li>
-              <li><button className="text-gray-500 hover:text-green-500 hover:bg-green-50 px-4 py-2 rounded transition-all duration-200 ease-in-out">Save</button></li>
-        </menu>
+    return (
+        <div className="w-[35rem] mt-16 px-8">
+            <menu className='flex items-center justify-end gap-4 my-4'>
+                <li><Button variant="text" color="danger">Cancel</Button></li>
+                <li><Button variant="text" color="success">Save</Button></li>
+            </menu>
 
-        <div>
-            <Input label="Title" />
-        <Input label="Description" isTextarea={true} />
-            <Input label="Due Date" />
+            <div>
+                <Input label="Title" />
+                <Input label="Description" isTextarea={true} />
+                <Input label="Due Date" />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
