4f05263ee5143b8b6420c7dddfdc0ca19bb9b188
James Mills <james.mills@lontra.co.uk>
2024-03-17 08:52:47 +0000

    Setup references for the form as it's a separate component.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,14 @@
-export default function Input({ label, isTextarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, isTextarea, ...props }, ref) {
     const formClasses = "w-full shadow border rounded px-3 py-2 leading-tight text-stone-700 bg-stone-200 border-stone-300 focus:outline-none focus:border-stone-600 transition-all duration-200 ease-in-out";
 
     return (
        <p className="flex flex-col gap-2 my-4">
            <label className="text-sm font-semibold text-gray-600">{label}</label>
-            {isTextarea ? <textarea className={formClasses} {...props} /> : <input className={formClasses} {...props} />}
+            {isTextarea ? <textarea ref={ref} className={formClasses} {...props} /> : <input ref={ref} className={formClasses} {...props} />}
        </p>
     );
-}
+});
+
+export default Input;
--- src/partials/Project/NewProject.jsx
+++ src/partials/Project/NewProject.jsx
@@ -1,7 +1,13 @@
+import { useRef } from 'react';
+
 import Input from '../../components/Input.jsx'
 import Button from '../../components/Button.jsx'
 
 export default function NewProject() {
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+    const dueDateRef = useRef();
+
     return (
         <div className="w-[35rem] mt-16 px-8">
             <menu className='flex items-center justify-end gap-4 my-4'>
@@ -10,9 +16,9 @@ export default function NewProject() {
             </menu>
 
             <div>
-                <Input label="Title" />
-                <Input label="Description" isTextarea={true} />
-                <Input label="Due Date" />
+                <Input ref={titleRef} label="Title" />
+                <Input ref={descriptionRef} label="Description" isTextarea={true} />
+                <Input ref={dueDateRef} label="Due Date" />
             </div>
         </div>
     )
